f249323c0d737cc4e6ba09619cfc06659ab20d9b
Asia <asiashahzadawan1@gmail.com>
2024-04-30 18:09:05 +0500

    first commit

---
+++ loop.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+for (let i = 1; i <= 10; i++) {
+    let tableNumber = 10;
+    console.log(tableNumber, "x", i, "=", tableNumber * i);
+}
+console.log(".........................");
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log("------------------------");
+for (let i = 1; i <= 10; i++) {
+    let tableNumber = 7;
+    console.log(tableNumber, "x", i, "=", tableNumber * i);
+}
+console.log(",,,,,,,,,,,,,,,,,,,,,,,,,,,");
+const girls = ["sana", "ayesha", "sara", "amna",];
+let index = 1;
+for (let girl of girls) {
+    console.log(`my ${index} sister is ${girl}`);
+}
+console.log("..............................");
+let girls2 = ["amna", "sara", "ayesha", "noreen",];
+for (let girl of girls) {
+    console.log(girl);
+}
+console.log("==================================");
+let i = 1;
+while (i <= 10) {
+    console.log(5, "x", i, "=", 5 * i);
+    i++;
+}
+console.log(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,");
+let j = 11;
+do {
+    console.log(7, "x", j, "=", 7 * j, "line33");
+    j++;
+} while (j <= 10);
+export {};
